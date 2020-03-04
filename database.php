fc7d4a3153cd054f35edf3741ecd80e9d09d8e92
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-04 16:14:29 +0100

    up

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php 
+  include 'env.php';
+
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  if ($conn && $conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+  }
+  $sql = "SELECT * FROM `stanze`";
+  $result = $conn->query($sql);
+  if ($result && $result->num_rows > 0) {
+    $rooms = [];
+    while($row = $result->fetch_assoc()) {
+      $rooms[] = $row;
+    }
+    var_dump($rooms); die();
+  } elseif ($result) {
+    echo "0 results";
+  } else {
+    echo "query error";
+  }
+  $conn->close();
+  
+?>
